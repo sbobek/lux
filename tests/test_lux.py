7c013248a38d5aef77f8df17048b681573201f4d
Szymon Bobek <szymon.bobek@gmail.com>
2025-01-14 09:52:58 +0100

    Added unit tests to be filled (pytest support)

---
+++ tests/test_lux.py
@@ -0,0 +1,33 @@
+class TestLux:
+    def test_fit(self):
+        assert False
+
+    def test_fit_bounding_boxes(self):
+        assert False
+
+    def test_create_sample_bb(self):
+        assert False
+
+    def test_process_and_predict_proba(self):
+        assert False
+
+    def test_process_input(self):
+        assert False
+
+    def test_predict(self):
+        assert False
+
+    def test_justify(self):
+        assert False
+
+    def test_counterfactual(self):
+        assert False
+
+    def test_visualize(self):
+        assert False
+
+    def test_to_hmr(self):
+        assert False
+
+    def test_generate_uarff(self):
+        assert False
---
+++ tests/test_samplers.py
@@ -0,0 +1,14 @@
+class TestImportanceSampler:
+    def test_fit(self):
+        assert False
+
+    def test_transform(self):
+        assert False
+
+
+class TestUncertainSMOTE:
+    def test__fit_resample(self):
+        assert False
+
+    def test__in_danger_noise(self):
+        assert False
